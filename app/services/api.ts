496965e8300cbb1f86a6469662f3590af1339650
VinsDev <vinsdev185@gmail.com>
2024-07-16 20:49:00 +0100

    url

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://jostum-engineering-journal-backend.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
